44839a45fedbc615a77f5c64a8d4da6c8b3fe792
unknown <quino.terrasa+dev@gmail.com>
2020-08-03 22:10:15 +0200

    Version is updated to 0.2. Now random queries truly work!\n(first 2-3 queries to the endpoint fail - I'm using a cumbersome implementation :( )

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -2,8 +2,9 @@ mod film;
 mod player;
 
 use actix_web::{web, HttpResponse, Responder};
-use mongodb::{Collection, bson::doc};
+use mongodb::{Collection, bson::doc, bson::Document};
 use serde::Deserialize;
+use futures::StreamExt;
 // use player::Player;
 // use film::Film;
 
@@ -22,15 +23,34 @@ pub struct Info {
 // GET /player/random
 pub async fn get_random_player(app_data: web::Data<DBState>) -> impl Responder {
     // Player { name: "Charles Chaplin", birth_year: 1889, death_year: 1977 }
-    let result = &app_data.crew_coll.find_one(doc! {}, None).await;
-
+    let db_query = doc! { "$sample": doc! { "size": 1 } };
+    let pipeline = vec![db_query];
+    
+    let result = (&app_data).crew_coll.aggregate(pipeline, None).await;
+    
     match result {
-        Ok(doc) => {
-            let body = serde_json::to_string(&doc).unwrap();
-            return HttpResponse::Ok().content_type("application/json").body(body);
+        Ok(cursor) => {
+            let docs: Vec<Result<Document, mongodb::error::Error>> = cursor.collect().await;
+
+            if docs.len() > 0 {
+                let doc: &Document;
+                match docs[0].as_ref() {
+                    Ok(val) => { doc = val },
+                    Err(e) => { 
+                        eprintln!("{:?}", e);
+                        return HttpResponse::BadRequest().finish();
+                    }
+                }
+                let doc = serde_json::to_string(doc).unwrap();
+                HttpResponse::Ok().content_type("application/json").body(&doc)
+            } else {
+                eprintln!("(/film/random/) Cursor is empty");
+                HttpResponse::BadRequest().finish()
+            }
         },
-        Err(_) => {
-            return HttpResponse::InternalServerError().finish();
+        Err(e) => {
+            eprintln!("{:?}", e);
+            HttpResponse::BadRequest().finish()
         },
     }
 }
@@ -38,16 +58,34 @@ pub async fn get_random_player(app_data: web::Data<DBState>) -> impl Responder {
 // GET /film/random
 pub async fn get_random_film(app_data: web::Data<DBState>) -> impl Responder {
     // Film { title: "The Great Dictator", year: 1940 }
-    // let db_query = doc! { "$sample": doc! { "size": 1 } };
-    let result = &app_data.film_coll.find_one(doc! {}, None).await;
-
+    let db_query = doc! { "$sample": doc! { "size": 1 } };
+    let pipeline = vec![db_query];
+    
+    let result = (&app_data).film_coll.aggregate(pipeline, None).await;
+    
     match result {
-        Ok(doc) => {
-            let body = serde_json::to_string(&doc).unwrap();
-            return HttpResponse::Ok().content_type("application/json").body(body);
+        Ok(cursor) => {
+            let docs: Vec<Result<Document, mongodb::error::Error>> = cursor.collect().await;
+
+            if docs.len() > 0 {
+                let doc: &Document;
+                match docs[0].as_ref() {
+                    Ok(val) => { doc = val },
+                    Err(e) => { 
+                        eprintln!("{:?}", e);
+                        return HttpResponse::BadRequest().finish();
+                    }
+                }
+                let doc = serde_json::to_string(doc).unwrap();
+                HttpResponse::Ok().content_type("application/json").body(&doc)
+            } else {
+                eprintln!("(/film/random/) Cursor is empty");
+                HttpResponse::BadRequest().finish()
+            }
         },
-        Err(_) => {
-            return HttpResponse::InternalServerError().finish();
+        Err(e) => {
+            eprintln!("{:?}", e);
+            HttpResponse::BadRequest().finish()
         },
     }
 }
